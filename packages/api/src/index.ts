2ffadc9e670e35884b3273335f2c038482961982
mateojovani <mateojovani@yahoo.com>
2020-08-22 21:51:23 +0100

    Apollo server express setup

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello')
+import { ApolloServer, gql } from 'apollo-server-express'
+import express from 'express'
+
+const app = express()
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type CV {
+      title: String
+      author: String
+    }
+
+    type Query {
+      cvs: [CV]
+    }
+  `,
+  resolvers: {
+    Query: {
+      cvs: () => [],
+    },
+  },
+})
+
+server.applyMiddleware({ app })
+
+app.listen({ port: process.env.API_PORT || 9000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
